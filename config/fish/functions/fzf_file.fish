8063ad2416a580d93f51724c68a7341c169fd14e
Konrad Klawonn <k.klawonn@mailbox.org>
2024-08-28 09:13:10 +0200

    change fzf layout

--- config/fish/functions/fzf_file.fish
+++ config/fish/functions/fzf_file.fish
@@ -1,7 +1,7 @@
 function fzf_file
     set -l target (fdfind -tf --hidden -E ".git" | fzf \
     --preview 'batcat --color=always --style=plain {}' \
-    --preview-window top:70% \
+    --preview-window bottom:60% \
     --reverse \
     --bind ctrl-p:toggle-preview \
     --header (pwd))
