def731e65e681f29f66edbe84a3bb4f128e3ec30
Konrad Klawonn <k.klawonn@mailbox.org>
2024-08-28 17:28:44 +0200

    fix colors

--- use_dotfiles.sh
+++ use_dotfiles.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/fish
 
 rm -rf ~/.config/fish ~/.tmux.conf ~/.vimrc ~/.zshrc ~/.config/bat 2> /dev/null
 mkdir ~/.config 2> /dev/null
-mkdir ~/.vim
+mkdir ~/.vim 2> /dev/null
 ln -s ~/.dotfiles/config/fish ~/.config/fish
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
