4f39e73e25ce6b8956572a2db1e28fd23ef5f387
Konrad Klawonn <k.klawonn@mailbox.org>
2025-09-12 17:21:50 +0200

    Add symlinks to neovim config

--- use_dotfiles.sh
+++ use_dotfiles.sh
@@ -1,6 +1,6 @@
-#!/bin/fish
+#!/bin/bash
 
-rm -rf ~/.config/fish ~/.tmux.conf ~/.vimrc ~/.zshrc ~/.config/bat 2>/dev/null
+rm -rf ~/.config/fish ~/.tmux.conf ~/.vimrc ~/.zshrc ~/.config/bat ~/.config/nvim 2>/dev/null
 mkdir ~/.config 2>/dev/null
 mkdir ~/.vim 2>/dev/null
 ln -s ~/.dotfiles/config/fish ~/.config/fish
@@ -9,4 +9,5 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/config/bat ~/.config/bat
 ln -s ~/.dotfiles/vim/colors ~/.vim/colors
+ln -s ~/.dotfiles/config/nvim/ ~/.config/nvim
 bat cache --build 2 /dev/null &>1
